8d52c2333a85ad4e90c71e15986112dab6a35bd5
alexkharlam <aakharlam@gmail.com>
2023-02-25 10:34:20 +0400

    fix: profile page bug

--- pages/profile/index.js
+++ pages/profile/index.js
@@ -3,9 +3,6 @@ import { useSelector } from "react-redux";
 
 function ProfilePage() {
   const router = useRouter();
-  const isAuthed = useSelector((state) => state.auth.isAuthed);
-
-  if (!isAuthed) router.replace("/profile/login");
 
   return <div>Your profile</div>;
 }
